b8b181937d118b9bf8ab645bbf18368bcda48ef8
adonismercado <adonis_mercado@ucne.edu.do>
2025-09-04 15:12:42 -0400

    Creando el contexto

---
+++ RegistroDeJugadores/DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using RegistroDeJugadores.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RegistroDeJugadores.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Jugadores> Jugadores { get; set; }
+    }
+}
