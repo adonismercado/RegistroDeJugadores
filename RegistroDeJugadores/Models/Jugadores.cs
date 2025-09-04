4af36742b5ed2ad14e95283887e0bb769aa3abf6
adonismercado <adonis_mercado@ucne.edu.do>
2025-09-04 15:11:36 -0400

    Creando el modelo Jugadores con sus propiedades

---
+++ RegistroDeJugadores/Models/Jugadores.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroDeJugadores.Models
+{
+    public class Jugadores
+    {
+        [Key]
+        public string JugadorId { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
+
+        public int Partidas { get; set; }
+    }
+}
