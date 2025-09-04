505b616a992354d16649ff62be55f3818d59f7e2
adonismercado <adonis_mercado@ucne.edu.do>
2025-09-04 15:26:58 -0400

    Creando el service JugadoresService y su metodo Insertar

---
+++ RegistroDeJugadores/Services/JugadoresService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroDeJugadores.DAL;
+using RegistroDeJugadores.Models;
+using System.Linq.Expressions;
+
+namespace RegistroDeJugadores.Services
+{
+    public class JugadoresService(IDbContextFactory<Contexto> DbFactory)
+    {
+        public async Task<bool> Insertar (Jugadores jugador)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            contexto.Jugadores.Add(jugador);
+            return await contexto.SaveChangesAsync() > 0;
+        }
+    }
+}
