97c7a640c73edf95ebe429f51a77862417d42d08
adonismercado <adonis_mercado@ucne.edu.do>
2025-09-08 20:24:20 -0400

    Usando file scoped namespace en el modelo Jugadores
    #2

--- RegistroDeJugadores/Models/Jugadores.cs
+++ RegistroDeJugadores/Models/Jugadores.cs
@@ -1,15 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace RegistroDeJugadores.Models
-{
+namespace RegistroDeJugadores.Models;
+
     public class Jugadores
-    {
-        [Key]
-        public int JugadorId { get; set; }
+{
+    [Key]
+    public int JugadorId { get; set; }
 
-        [Required(ErrorMessage = "El nombre es obligatorio")]
-        public string Nombre { get; set; } = null!;
+    [Required(ErrorMessage = "El nombre es obligatorio")]
+    public string Nombre { get; set; } = null!;
 
-        public int Partidas { get; set; } = 0;
-    }
+    public int Partidas { get; set; } = 0;
 }
+
