31e7df5cfd13e6f3e6d6bb5293d4555038b0efe7
adonismercado <adonis_mercado@ucne.edu.do>
2025-09-10 23:35:11 -0400

    Creando el servicio PartidasService con todos sus metodos: Insertar, Eliminar, Buscar, GetList, Guardar, Modificar y Existe

--- RegistroDeJugadores/DAL/Contexto.cs
+++ RegistroDeJugadores/DAL/Contexto.cs
@@ -7,6 +7,6 @@ public class Contexto : DbContext
 {
     public DbSet<Jugadores> Jugadores { get; set; }
     public DbSet<Partidas> Partidas { get; set; }
-    public Contexto(DbContextOptions<Contexto> options) : base(options) { } 
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 }
 
