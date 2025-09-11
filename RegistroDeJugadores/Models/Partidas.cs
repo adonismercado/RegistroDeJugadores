0abda7ce98cecbda2cb760245c3527d086d8e7aa
adonismercado <adonis_mercado@ucne.edu.do>
2025-09-11 11:17:09 -0400

    Cambiando PageName por PageTitle en PartidasIndex y corrigiendo errores de gramatica y eliminando card-body innecesario y añadiendo FormName en PartidasCreate para solucionar errores

--- RegistroDeJugadores/Models/Partidas.cs
+++ RegistroDeJugadores/Models/Partidas.cs
@@ -11,9 +11,9 @@ public class Partidas
     public int Jugador1Id { get; set; }
     public int? Jugador2Id { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "El EstadoPartida es obligatorio.")]
     [StringLength(20)]
-    public string EstadoPartida { get; set; }
+    public string EstadoPartida { get; set; } = string.Empty;
 
     public int? GanadorId { get; set; }
     public int TurnoJugadorId { get; set; }
