3f47787a3b4e461c5470841b43e8df5cc4f71369
adonismercado <adonis_mercado@ucne.edu.do>
2025-10-22 16:32:46 -0400

    Creando el servicio PartidasApiService, y su interfaz, ademas los metodos Post y Get

--- RegistroDeJugadores.BlazorWasm/Program.cs
+++ RegistroDeJugadores.BlazorWasm/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using RegistroDeJugadores.BlazorWasm;
+using RegistroDeJugadores.BlazorWasm.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://gestionhuacalesapi.azurewebsites.net/") });
+builder.Services.AddScoped<IPartidasApiService, PartidasApiService>();
 
 await builder.Build().RunAsync();
---
+++ RegistroDeJugadores.BlazorWasm/Services/IPartidasApiService.cs
@@ -0,0 +1,9 @@
+﻿using RegistroDeJugadores.Shared;
+using RegistroDeJugadores.Shared.Dtos;
+
+public interface IPartidasApiService
+{
+    Task<Resource<List<PartidaResponse>>> GetPartidasAsync();
+    Task<Resource<PartidaResponse>> GetPartidaAsync(int partidaId);
+    Task<Resource<PartidaResponse>> PostPartida(int jugador1, int jugador2);
+}
