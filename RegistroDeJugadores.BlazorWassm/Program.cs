0424344a1f9edb7eac963bcf7a5a1fa9f31f306a
adonismercado <adonis_mercado@ucne.edu.do>
2025-10-23 10:27:54 -0400

    Inyectando el MovimientosApiService en el program

--- RegistroDeJugadores.BlazorWassm/Program.cs
+++ RegistroDeJugadores.BlazorWassm/Program.cs
@@ -9,4 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://gestionhuacalesapi.azurewebsites.net/") });
 builder.Services.AddScoped<IPartidasApiService, PartidasApiService>();
+builder.Services.AddScoped<IMovimientosApiService, MovimientosApiService>();
+
 await builder.Build().RunAsync();
