f8c091a520fc004807d3552bf3d42986edf86e2f
adonismercado <adonis_mercado@ucne.edu.do>
2025-10-23 10:12:58 -0400

    Creando los DTOs MovimientosRequest y MovimientosResponse

---
+++ RegistroJugadores.Shared/Dtos/MovimientosResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace RegistroJugadores.Shared.Dtos;
+
+public record MovimientosResponse(
+    int PartidaId,
+    string Jugador,
+    int PosicionFila,
+    int PosicionColumna);
