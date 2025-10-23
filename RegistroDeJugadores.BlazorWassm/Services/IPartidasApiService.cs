ade439d7b0a7a0a88a351b112366b54837e44db5
adonismercado <adonis_mercado@ucne.edu.do>
2025-10-23 09:01:00 -0400

    Creando la interfaz del servicio

---
+++ RegistroDeJugadores.BlazorWassm/Services/IPartidasApiService.cs
@@ -0,0 +1,11 @@
+﻿using RegistroJugadores.Shared;
+using RegistroJugadores.Shared.Dtos;
+
+namespace RegistroDeJugadores.BlazorWassm.Services;
+
+public interface IPartidasApiService
+{
+    Task<Resource<List<PartidasResponse>>> GetPartidasAsync();
+    Task<Resource<PartidasResponse>> GetPartidaAsync(int partidaId);
+    Task<Resource<PartidasResponse>> PostPartida(int jugador1, int jugador2);
+}
