5df82e314bc1820c8dd2274732cd4476b7cd4848
adonismercado <adonis_mercado@ucne.edu.do>
2025-10-23 10:24:37 -0400

    Creando el servicio MovimientosApiService con sus metodos GetById y Post

---
+++ RegistroDeJugadores.BlazorWassm/Services/MovimientosApiService.cs
@@ -0,0 +1,41 @@
+﻿using RegistroJugadores.Shared;
+using RegistroJugadores.Shared.Dtos;
+using System.Net.Http.Json;
+
+namespace RegistroDeJugadores.BlazorWassm.Services;
+
+public class MovimientosApiService (HttpClient httpClient)
+{
+    public async Task<Resource<MovimientosResponse[]>> GetMovimientosAsync(int partidaId)
+    {
+        try
+        {
+            var response = await httpClient.GetFromJsonAsync<MovimientosResponse[]>($"api/Movimientos/{partidaId}");
+            return new Resource<MovimientosResponse[]>.Success(response!);
+        }
+        catch (Exception ex)
+        {
+            return new Resource<MovimientosResponse[]>.Error(ex.Message);
+        }
+    }
+
+    public async Task<Resource<bool>> PostMovimientos(int partidaId, string jugador, int posicionFila, int posicionColumna)
+    {
+        var request = new MovimientosRequest(partidaId, jugador, posicionFila, posicionColumna);
+        
+        try
+        {
+            var response = await httpClient.PostAsJsonAsync("api/Movimientos", request);
+            response.EnsureSuccessStatusCode();
+            return new Resource<bool>.Success(true);
+        }
+        catch (HttpRequestException ex)
+        {
+            return new Resource<bool>.Error($"Error de red: {ex.Message}");
+        }
+        catch (NotSupportedException)
+        {
+            return new Resource<bool>.Error("Respuesta invalida del servidor.");
+        }
+    }
+}
