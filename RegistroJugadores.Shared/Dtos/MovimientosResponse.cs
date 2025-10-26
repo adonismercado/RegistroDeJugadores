da0d89aac111d8ea9b8d2c40245baacb697c73f5
adonismercado <adonis_mercado@ucne.edu.do>
2025-10-26 13:42:09 -0400

    Revert "Tarea 6"

--- RegistroJugadores.Shared/Dtos/MovimientosResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Security.Cryptography.X509Certificates;
-
-namespace RegistroJugadores.Shared.Dtos;
-
-public record MovimientosResponse(
-    int PartidaId,
-    string Jugador,
-    int PosicionFila,
-    int PosicionColumna);
--- RegistroJugadores.Shared/Dtos/PartidasResponse.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace RegistroJugadores.Shared.Dtos;
-
-public record PartidasResponse(
-    int PartidaId,
-    int Jugador1Id,
-    int? Jugador2Id);
