1a97bb3f3fe757ff80afc2f2de3dc2c84b9dc393
adonismercado <adonis_mercado@ucne.edu.do>
2025-10-26 15:17:19 -0400

    Creando el servicio MovimientosApiService

---
+++ RegistroDeJugadores.BlazorWasm/Services/MovimientosApiService.cs
@@ -0,0 +1,42 @@
+﻿using RegistroDeJugadores.Shared;
+using RegistroDeJugadores.Shared.Dtos;
+using System.Net.Http.Json;
+
+namespace RegistroDeJugadores.BlazorWasm.Services
+{
+    public class MovimientosApiService(HttpClient httpClient) : IMovimientosApiService
+    {
+        public async Task<Resource<List<MovimientoResponse>>> GetMovimientos(int partidaId)
+        {
+            try
+            {
+                var response = await httpClient.GetFromJsonAsync<List<MovimientoResponse>>($"api/Movimientos/{partidaId}");
+                return new Resource<List<MovimientoResponse>>.Success(response ?? []);
+            }
+            catch (Exception ex)
+            {
+                return new Resource<List<MovimientoResponse>>.Error(ex.Message);
+            }
+        }
+
+        public async Task<Resource<MovimientoResponse>> PostMovimientos(int partidaId, string jugador, int posicionFila, int posicionColumna)
+        {
+            var request = new MovimientoRequest(partidaId, jugador, posicionFila, posicionColumna);
+
+            try
+            {
+                var response = await httpClient.PostAsJsonAsync("api/Movimientos", request);
+                response.EnsureSuccessStatusCode();
+                return new Resource<MovimientoResponse>.Success(null!);
+            }
+            catch (HttpRequestException ex)
+            {
+                return new Resource<MovimientoResponse>.Error($"Error de red: {ex.Message}");
+            }
+            catch (NotSupportedException)
+            {
+                return new Resource<MovimientoResponse>.Error("Respuesta inválida del servidor.");
+            }
+        }
+    }
+}
